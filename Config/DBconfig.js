e6241c6c07b8d547bad494eea2d63a4eb97764ef
youssef <youssefemadhassan66@gmail.com>
2024-09-09 08:15:41 +0300

    Changed Db from local to Atlas cloud

--- Config/DBconfig.js
+++ Config/DBconfig.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-
-const ConnectionDB = async()=>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/MyPortfolio");
-    console.log('Database is connected ');
+const ConnectionDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Database is connected');
+    } catch (error) {
+        console.error('Database connection error:', error);
+    }
 }
-
 
 module.exports = ConnectionDB;
